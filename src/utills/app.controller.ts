ef4ad5dc6947ce19921a6aaad2a6b3ffd504e5c8
Mahdi gosyan <mahdigosyanapple@gmail.com>
2023-04-14 23:05:16 +0330

    Update app.controller.ts

--- src/utills/app.controller.ts
+++ src/utills/app.controller.ts
@@ -6,3 +6,14 @@ import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { DallorSchema, Dallor } from './dallor.schema';
 
+
+@Module({
+    imports: [
+      MongooseModule.forRoot("mongodb://localhost:27017/dallor"),
+      HttpModule.register({
+      timeout: 5000,
+      maxRedirects: 5,
+      httpsAgent: new Agent({
+        rejectUnauthorized: false
+      }),
+      
