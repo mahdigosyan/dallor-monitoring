64983add36054b2fd768e1e7735918a78cf4dff0
Mahdi gosyan <mahdigosyanapple@gmail.com>
2023-04-16 12:18:04 +0330

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,3 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {DocumentBuilder, SwaggerModule} from "@nestjs/swagger"
 
+sync function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+    const config = new DocumentBuilder().setTitle("Dallor Monitoring").setDescription("....").build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup("/", app, document)
+    await app.listen(3000);
+  }
+  bootstrap();
+
+  
